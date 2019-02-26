6bd8c2738baeaf2108f1bb734fc5c9aa54665e43
Matthew Wright <matthewbwright@gmail.com>
2019-02-26 16:12:50 -0700

    completes BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,14 +1,55 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        current = self
+        given = value
+        complete = False
+        while not complete:
+            if given < current.value:
+                # add value to the left node
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = BinarySearchTree(given)
+                    complete = True
+            if given > current.value:
+                # add value to the right node
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = BinarySearchTree(given)
+                    complete = True
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        current = self
+        complete = False
+        while not complete:
+            # if there is nothing left to check, return False
+            if not current:
+                return False
+            # if the value of the current node is == target
+            if current.value == target:
+                return True
+            # if the current value is greater than the target
+            elif current.value > target:
+                current = current.left
+            # if current value is less than the target
+            else:
+                current = current.right
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        current = self
+        max = 0
+        while current:
+          # if the current value is greater than the mx
+            if current.value > max:
+                max = current.value
+                current = current.right
+            # if current value is less than the max value
+            else:
+                current = current.right
+        return max
